962255f7c3a4704d06c51c5df0493189afe280ad
Allnetwark <allnetwark.com@gmail.com>
2024-10-19 16:05:08 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=HuR2BADS#PLBPp6Spzjx2CmcR7VDEF1J2xSzYoVKL6VC6-qzsYc4",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
